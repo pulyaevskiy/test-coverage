25cc3007622dcb81f19fcf457d4c5943a07eca7b
Dell Green <dell.green@ideaworks.co.uk>
2020-11-15 19:55:55 +0000

    Add option to import all source files for test processing. (#28)

--- test/test_coverage_test.dart
+++ test/test_coverage_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:glob/glob.dart';
 import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 import 'package:test_coverage/test_coverage.dart';
@@ -7,6 +8,35 @@ import 'package:test_coverage/test_coverage.dart';
 void main() {
   final stubPath = path.join(Directory.current.path, 'test', 'stub_package');
   final stubDir = Directory(stubPath);
+
+  group('getPackageName', () {
+    test('get package name', () {
+      expect(getPackageName(stubDir), 'stub_package');
+    });
+  });
+
+  group('findSourceFiles', () {
+    test('finds only source files', () {
+      final result = findSourceFiles(stubDir);
+      print(result);
+      expect(result, hasLength(2));
+      final filenames =
+          result.map((f) => f.path.split(path.separator).last).toList();
+      expect(filenames, contains('stub_package.dart'));
+      expect(filenames, contains('stub_package2.dart'));
+    });
+
+    test('finds only source files with glob', () {
+      final result =
+          findSourceFiles(stubDir, excludeGlob: Glob('**stub_package2.dart'));
+      print(result);
+      expect(result, hasLength(1));
+      final filenames =
+          result.map((f) => f.path.split(path.separator).last).toList();
+      expect(filenames[0], 'stub_package.dart');
+    });
+  });
+
   group('findTestFiles', () {
     test('finds only test files', () {
       final result = findTestFiles(stubDir);
@@ -49,17 +79,42 @@ void main() {
       final content = testFile.readAsStringSync();
       expect(content, contains('a_test.main();'));
       expect(content, contains('nested_b_test.main();'));
+      expect(content,
+          isNot(contains('import \'package:stub_package/src/stub_package2.dart\';')));  
+      expect(content,
+          isNot(contains('import \'package:stub_package/stub_package.dart\';')));
 
       // Set custom port so that when running test_coverage for this test
       // we can start another Observatory for stub_package on the default port.
       await runTestsAndCollect(stubPath, '8585');
 
       expect(lcovFile.existsSync(), isTrue);
-
       final coverageValue = calculateLineCoverage(lcovFile);
       expect(coverageValue, 1.0);
       generateBadge(stubDir, coverageValue);
+      expect(badgeFile.existsSync(), isTrue);
+    });
 
+    test('generate script with source files', () async {
+      final files = findTestFiles(stubDir);
+      final srcFiles = findSourceFiles(stubDir);
+      generateMainScript(stubDir, files, sourceFiles: srcFiles);
+      expect(testFile.existsSync(), isTrue);
+      final content = testFile.readAsStringSync();
+      expect(content, contains('a_test.main();'));
+      expect(content, contains('nested_b_test.main();'));
+      expect(content,
+          contains('import \'package:stub_package/src/stub_package2.dart\';'));
+      expect(content,
+          contains('import \'package:stub_package/stub_package.dart\';'));
+
+      // Set custom port so that when running test_coverage for this test
+      // we can start another Observatory for stub_package on the default port.
+      await runTestsAndCollect(stubPath, '8585');
+      expect(lcovFile.existsSync(), isTrue);
+      final coverageValue = calculateLineCoverage(lcovFile);
+      expect(coverageValue, 0.5);
+      generateBadge(stubDir, coverageValue);
       expect(badgeFile.existsSync(), isTrue);
     });
   });
